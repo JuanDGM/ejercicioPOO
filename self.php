8f48c6aaa662f410d7d3a0ceafe3ff16afa8e661
Juan David Garcia <juan84_10@hotmail.com>
2021-05-15 19:41:41 -0500

    Se incluyo el self

---
+++ self.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Metodo{
+
+    public function mandarColor($valor){
+
+        if($valor==1){
+            return "rojo";
+        }else if($valor==2){
+            return "Amarillo";
+        }else if($valor==3){
+            return "Azul";
+        }
+    }
+
+    public function darAltaColor($tipoColor){
+
+        return self::mandarColor($tipoColor);
+
+    }
+
+}
+
+
+$obj = new Metodo();
+
+echo $obj->darAltaColor(3);
+
+?>
